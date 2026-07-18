0a3e3ee42c3312707010d1dd48bae101979fc2c6
ECanDo <eric.opseth@gmail.com>
2026-07-17 22:33:03 -0600

    Some uci info output

--- src/search/search_types.rs
+++ src/search/search_types.rs
@@ -3,6 +3,7 @@ use std::sync::{
     atomic::{AtomicU64, Ordering},
     Arc,
 };
+use std::time::Instant;
 
 pub struct SharedData {
     // tt
@@ -11,6 +12,7 @@ pub struct SharedData {
 pub struct SearchData {
     pub board: Board,
     pub shared_data: Arc<SharedData>,
+    pub start_time: Instant,
 }
 
 pub struct Counter {
@@ -48,15 +50,48 @@ impl SearchData {
         Self {
             board: Board::startpos(),
             shared_data: shared,
+            start_time: Instant::now(),
         }
     }
 
-    pub fn set_board(&mut self, board: &Board){
+    pub fn set_board(&mut self, board: &Board) {
         self.board = board.clone();
     }
 
     pub fn nodes(&self) -> u64 {
         self.shared_data.nodes.count()
+    }
+
+    pub fn reset_clock(&mut self) {
+        self.start_time = Instant::now();
+    }
+
+    pub fn elapsed_ms(&self) -> u128 {
+        self.start_time.elapsed().as_millis()
+    }
+
+    pub fn elapsed_us(&self) -> u128 {
+        self.start_time.elapsed().as_micros()
+    }
+
+    pub fn elapsed_ns(&self) -> u128 {
+        self.start_time.elapsed().as_nanos()
+    }
+
+    pub fn to_uci_info(&self, depth: i32) -> String {
+        let time_ms = self.elapsed_ms();
+        let nodes = self.nodes();
+        let nps: u128 = (nodes as u128) * 1_000_000 / self.elapsed_us();
+
+        let mut uci_out = String::from(format!("info depth {} ", depth));
+
+        uci_out += format!("nodes {} ", nodes).as_str();
+
+        uci_out += format!("time {} ", time_ms).as_str();
+
+        uci_out += format!("nps {} ", nps).as_str();
+
+        uci_out
     }
 }
 
